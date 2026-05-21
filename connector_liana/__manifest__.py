5791556679a46860d1d8e05e7b4fc06a00c602ca
Mikko Närjänen <mikko.narjanen@avoin.systems>
2026-05-21 15:18:38 +0300

    [ADD] 19.0 connector_liana

---
+++ connector_liana/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    "name": "Liana Connector",
+    "version": "19.0.1.0.0",
+    "category": "Marketing",
+    "author": "Liana Technologies",
+    "license": "Other proprietary",
+    "website": "https://www.lianatech.com/",
+    "depends": [
+        "base_automation",
+        "crm",
+        "sale",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_sequence_data.xml",
+        "views/ir_actions_server_views.xml",
+        "views/liana_backend_views.xml",
+        "views/liana_event_views.xml",
+        "views/liana_automation_views.xml",
+        "views/liana_dashboard_views.xml",
+        "views/crm_lead_views.xml",
+        "views/res_partner_views.xml",
+        "views/menus.xml",
+        "data/default_automations.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False  # Don't install when all dependencies are satisfied
+}
